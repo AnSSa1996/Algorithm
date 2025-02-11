47ee7ec41392893ad67b333f6b0f03f4039d2c9d
AnSSa1996 <48550461+AnSSa1996@users.noreply.github.com>
2025-02-12 08:41:21 +0900

    Create 25083 새싹.cs

---
+++ Backjun/B5/25083 새싹.cs
@@ -0,0 +1,12 @@
+var sr = new StreamReader(Console.OpenStandardInput());
+var sw = new StreamWriter(Console.OpenStandardOutput());
+
+sw.WriteLine(@"         ,r'""7
+r`-_   ,'  ,/
+ \. "". L_r'
+   `~\/
+      |
+      |");
+
+sw.Close();
+sr.Close();
